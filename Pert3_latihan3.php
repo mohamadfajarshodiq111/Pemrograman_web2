15763b5c97fa59e0c79df6360142cbd2e6b9881a
mohamadfajarshodiq111 <80590341+mohamadfajarshodiq111@users.noreply.github.com>
2021-03-13 23:22:53 +0700

    Update Pert3_latihan3.php

--- Pert3_latihan3.php
+++ Pert3_latihan3.php
@@ -1,32 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Penggunaan IF Statement</title>
-</head>
-<body>
-    <form>
-        Besar Pembelian :
-        <input type="text" name="total_beli"><br>
-        <input type="submit" value="Tentukan Diskon">
-    </form>
-    <?php 
-        if (isset($total_beli)) {
-            $total_beli = intval($total_beli);
-            $discon = 0;
-            if ($total_beli >= 200000){
-                $discon = 0.1;
-            } elseif ($total_beli >= 100000) {
-                $discon = 0.05;
-            } else {
-                $discon = 0.01;
-            }
-            $discon = $discon * intval($total_beli);
-            printf("Diskon = %s \n", $discon);
-            printf("Pembayaran = %s \n", $total_beli - $discon);
-        }
-    ?>
-</body>
-</html>
+<?php 
+    $destination = "Tokyo";
+    switch ($destination) {
+        case "las Vegas":
+            echo "Bring an Extra $500";
+            break;
+        case "Amsterdam":
+            echo "Bring an open mind";
+            break;
+        case "Egypt":
+            echo "Bring 15 bottle of SPF 50 Sunscreen";
+            break;
+        case "Tokyo":
+            echo "Bring lots of Money";
+            break;
+        case "Carrabean Islands":
+            echo "Bring a Swimsuit";
+            break;
+    }
+?>
